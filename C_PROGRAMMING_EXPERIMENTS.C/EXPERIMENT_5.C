82caa6772e620be3a00abbe423f528d134d4adca
Ritesh Kumar Mahto <riteshkumar6201915393@gmail.com>
2026-01-03 01:08:49 +0530

    add c practical experiment

---
+++ C_PROGRAMMING_EXPERIMENTS.C/EXPERIMENT_5.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    float b = 5.5;
+    char c = 'A';
+    double d = 25.6789;
+
+    printf("Integer value: %d\n", a);
+    printf("Float value: %f\n", b);
+    printf("Character value: %c\n", c);
+    printf("Double value: %lf\n", d);
+
+    return 0;
+}
